f6a708765ef12058efb8baec6faeb29f01c3a20c
z10nn <z10nn@users.noreply.github.com>
2020-07-15 06:42:38 -0300

    add exercises from ReactJS course

---
+++ Starter/ReactJS/huntweb/build/precache-manifest.0a06fbede8f10d81d1ab20f94617ae89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcace49657e27cc64f028c8bc4cff238",
+    "url": "/Rocketseat/Starter/ReactJS/huntweb/build/index.html"
+  },
+  {
+    "revision": "30d01f617706bb63d83c",
+    "url": "/Rocketseat/Starter/ReactJS/huntweb/build/static/css/main.f635c813.chunk.css"
+  },
+  {
+    "revision": "e74b7797f1d67e4043d5",
+    "url": "/Rocketseat/Starter/ReactJS/huntweb/build/static/js/2.6db76f4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Rocketseat/Starter/ReactJS/huntweb/build/static/js/2.6db76f4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d01f617706bb63d83c",
+    "url": "/Rocketseat/Starter/ReactJS/huntweb/build/static/js/main.8a9d5d72.chunk.js"
+  },
+  {
+    "revision": "57d9748d6ee003334343",
+    "url": "/Rocketseat/Starter/ReactJS/huntweb/build/static/js/runtime-main.1f5218af.js"
+  }
+]);
